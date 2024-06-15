a410646e9f27cf7fc251b3fd3cb9fef51e627fa2
Pavan Kumar V <pkvstarscream@gmail.com>
2024-06-15 17:34:12 +0530

    views.py to manage and visualize employee data

--- project_pygal/testapp/charts.py
+++ project_pygal/testapp/charts.py
@@ -18,3 +18,37 @@ class EmployeePieChart():
             self.chart.add(key, value)
         return self.chart.render(is_unicode=True)
         
+class EmployeeGaugeChart():
+    def __init__(self, **kwargs):
+        self.chart = pygal.Gauge(**kwargs)
+        self.chart.title = 'Employees in different department'
+
+    def get_data(self):
+        data = {}
+        for emp in Employee.objects.all():
+            data[emp.department] = emp.strength
+        return data
+    
+    def generate(self):
+        chart_data = self.get_data()
+        for key, value in chart_data.items():
+            self.chart.add(key, value)
+        return self.chart.render(is_unicode=True)
+    
+class EmployeeBarChart():
+    def __init__(self, **kwargs):
+        self.chart = pygal.Bar(**kwargs)
+        self.chart.title = 'Employees in different department'
+
+    def get_data(self):
+        data = {}
+        for emp in Employee.objects.all():
+            data[emp.department] = emp.strength
+        return data
+    
+    def generate(self):
+        chart_data = self.get_data()
+        for key, value in chart_data.items():
+            self.chart.add(key, value)
+        return self.chart.render(is_unicode=True)
+    
--- project_pygal/testapp/views.py
+++ project_pygal/testapp/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+from pygal.style import DarkStyle # 'DarkStyle' -> changeable
+from django.http import HttpResponse
+from testapp.models import Employee
+from .charts import EmployeePieChart, EmployeeGaugeChart, EmployeeBarChart
 
 # Create your views here.
+def clear(request):
+    Employee.objects.all().delete()
+    return redirect('/home')
+
+def home(request):
+    if request.method == 'POST':
+        department = request.POST['department']
+        strength = request.POST['strength']
+        print(department)
+        print(strength)
+        Employee.objects.create(department=department, strength=strength)
+
+    return render(request, 'home.html')
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+
+        # Instantiate our chart. We'll keep the size/style/etc.
+        # config here in the view instead of 'charts.py'
+        cht_employee = EmployeePieChart(
+            height = 600,
+            width = 800,
+            explicit_size = True,
+            style = DarkStyle
+        )
+
+        # Call the '.generate()' method on our chart object
+        # and pass it to template context.
+        context['cht_employee'] = cht_employee.generate()
+        return context
+
+class IndexView1(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView1, self).get_context_data(**kwargs)
+
+        # Instantiate our chart. We'll keep the size/style/etc.
+        # config here in the view instead of 'charts.py'
+        cht_employee = EmployeeGaugeChart(
+            height = 600,
+            width = 800,
+            explicit_size = True,
+            style = DarkStyle
+        )
+
+        # Call the '.generate()' method on our chart object
+        # and pass it to template context.
+        context['cht_employee'] = cht_employee.generate()
+        return context
+    
+class IndexView2(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView2, self).get_context_data(**kwargs)
+
+        # Instantiate our chart. We'll keep the  size/style
