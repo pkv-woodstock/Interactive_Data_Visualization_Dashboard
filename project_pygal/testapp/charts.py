721e114973e7c8505594378143ef490764d16a23
Pavan Kumar V <pkvstarscream@gmail.com>
2024-06-15 17:01:26 +0530

    chart.py within app that extracts data from model and depicts graph. This functionality is written in here

---
+++ project_pygal/testapp/charts.py
@@ -0,0 +1,20 @@
+import pygal
+from .models import Employee
+
+class EmployeePieChart():
+    def __init__(self, **kwargs):
+        self.chart = pygal.Pie(**kwargs)
+        self.chart.title = 'Employees in different department'
+
+    def get_data(self):
+        data = {}
+        for emp in Employee.objects.all():
+            data[emp.department] = emp.strength
+        return data
+    
+    def generate(self):
+        chart_data = self.get_data()
+        for key, value in chart_data.items():
+            self.chart.add(key, value)
+        return self.chart.render(is_unicode=True)
+        
