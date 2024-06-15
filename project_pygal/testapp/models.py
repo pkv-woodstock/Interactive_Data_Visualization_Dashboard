0d6bec4f99baf5684b74a78c4ed9316228f3ed88
Pavan Kumar V <pkvstarscream@gmail.com>
2024-06-15 17:37:58 +0530

    update models.py

--- project_pygal/testapp/models.py
+++ project_pygal/testapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    department = models.CharField(max_length=255)
+    strength = models.IntegerField()
+
+    def __str__(self):
+        return self.department
--- project_pygal/testapp/views.py
+++ project_pygal/testapp/views.py
@@ -66,4 +66,16 @@ class IndexView2(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(IndexView2, self).get_context_data(**kwargs)
 
-        # Instantiate our chart. We'll keep the  size/style
+        # Instantiate our chart. We'll keep the  size/style etc.
+        # config here in the view instead of charts.py
+        cht_employee = EmployeeBarChart(
+            height = 600, 
+            width = 800,
+            explicit_size = True,
+            style = DarkStyle
+        )
+
+        # Call the '.generate()' method on our chart object
+        # and pass it to template context
+        context['cht_employee'] = cht_employee.generate()
+        return context
